7d6155b8744875078433442c1ce64a4ae936d086
NicolleMP <nimendespoltosi46@gmail.com>
2025-01-20 17:36:22 -0300

    Criação de array e implementação de função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector("input").value;
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nome);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    let campo = document.querySelector("input");
+    campo.value = "";
+}
